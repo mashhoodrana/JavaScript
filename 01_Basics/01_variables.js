1d89b9148e7b4f638008371b348be32497fbee47
Mashhood Rana <themashhoodrana@gmail.com>
2025-10-27 02:55:40 +0500

    Create 01_variables.js

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const name = "897"
+let email = "abdullah@google.com"
+var number = "03145689216"
+university = "IST"
+
+//name = abdullah not allowed
+email = "info@ribaash.com"
+number = "0011"
+university = "NUST"
+
+console.table([email, number, university])
